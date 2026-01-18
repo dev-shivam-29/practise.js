f51f153bdd5032b70fc953ba7ecbdc1789e66ccc
Shivam Dubey <shivamdubey55349@gmail.com>
2026-01-18 09:54:10 +0530

    Update practise11.js

--- practise11.js
+++ practise11.js
@@ -1,11 +1,20 @@
-h1= document.querySelector("h1");
+// h1= document.querySelector("h1");
 
-setTimeout(()=> {
-    h1.style.color ="red";
-},2000);
-setTimeout(()=> {
-    h1.style.color ="orange";
-},3000);
-setTimeout(()=> {
-    h1.style.color ="green";
-},5000);
+// setTimeout(()=> {
+//     h1.style.color ="red";
+// },2000);
+// setTimeout(()=> {
+//     h1.style.color ="orange";
+// },3000);
+// setTimeout(()=> {
+//     h1.style.color ="green";
+// },5000);
+h1= document.querySelector("h1");
+function changeColor(color,delay){
+    setTimeout(() =>{
+        h1.style.color = color;
+    },delay);
+}
+changeColor("red",1000);
+changeColor("green",2000);
+changeColor("blue",3000);
