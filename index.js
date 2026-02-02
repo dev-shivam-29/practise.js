040ba95290020fc310d3283e894ab980426d2fab
Shivam Dubey <shivamdubey55349@gmail.com>
2026-02-03 03:09:51 +0530

    added practice files

---
+++ index.js
@@ -0,0 +1,6 @@
+/*console.log('i am comming from practiceprashantsir');
+console.log('i am comming from practiceprashantsir');
+console.log('i am comming from practiceprashantsir');*/
+
+
+
--- practise11.js
+++ practise11.js
@@ -9,12 +9,50 @@
 // setTimeout(()=> {
 //     h1.style.color ="green";
 // },5000);
-h1= document.querySelector("h1");
-function changeColor(color,delay){
-    setTimeout(() =>{
-        h1.style.color = color;
-    },delay);
+// h1= document.querySelector("h1");
+// function changeColor(color,delay){
+//     setTimeout(() =>{
+//         h1.style.color = colo3r;
+//     },delay);
+// }
+// changeColor("red",1000);
+// changeColor("green",2000);
+// changeColor("blue",3000);
+// h1= document.querySelector("h1");
+// setTimeout(()=> {
+//     h1.style.color ="red";
+// },1000);
+// setTimeout(()=> {
+//     h1.style.color ="green";
+// },2000);
+// setTimeout(()=> {
+//     h1.style.color ="yellow";
+// },4000);
+// let h1 = document.querySelector("h1");
+
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         if (nextColorChange) nextColorChange();{
+//         }
+//     }, delay);
+// }
+
+// changeColor("red", 1000, () => {
+//     changeColor("green", 1000, () => {
+//         changeColor("blue", 1000);
+//     });
+// });
+// function savetoDb(data){
+//     let internetspeed =Math.floor(Math.random() *10)+1;
+//     if(internetspeed >4){
+//         console.log("your data was saved:",data);
+//     }else{
+//         console.log("weak connection. data not saved");
+//     }
+// }
+async function greet(){
+    // return "hello world";
+    console.log ("hello world");
 }
-changeColor("red",1000);
-changeColor("green",2000);
-changeColor("blue",3000);
+greet();
